c37fa09060c2bc03263d6e0f490863cf8ee7f98b
HalitSta <93347768+HalitSta@users.noreply.github.com>
2021-12-07 22:51:56 +0100

    Add files via upload

---
+++ Kalkulatori/main.js
@@ -0,0 +1,33 @@
+function dis(v) 
+         { 
+             document.getElementById("result").value+=v 
+         } 
+           
+        
+         function solve() 
+         { 
+           let x = document.getElementById("result").value 
+           let y = eval(x) 
+             document.getElementById("result").value = y 
+         } 
+           
+        
+         function clr() 
+         { 
+             document.getElementById("result").value = "" 
+         } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
